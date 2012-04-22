f71685b47a5e7ae523054c5475754189b94c1b72
Thomas Klemm <mail@tklemm.eu>
2012-04-22 21:35:38 +0200

    writing documentation

--- app/controllers/repo_controller.rb
+++ app/controllers/repo_controller.rb
@@ -1,4 +1,7 @@
 class RepoController < ApplicationController
+  
+  # index
+  # display all repos
   def index
     @repos = Repo.order("watchers DESC")
 
@@ -7,6 +10,10 @@ class RepoController < ApplicationController
     end
   end
 
+  # show
+  # display one repo
+  # allow tags to be added
+  # display alternatives / options user might want to know about
   def show
     @repo = Repo.find_by_owner_and_name(params[:owner], params[:name])
     
@@ -15,6 +22,10 @@ class RepoController < ApplicationController
     end
   end
 
+  # add_repo
+  # get request to '/add_repo?url=https://github.com/thomasklemm/repos'
+  #   will add repo to this app
+  # in use in bookmarklet
   def add_repo
     url = params[:url]
 
@@ -33,11 +44,21 @@ class RepoController < ApplicationController
     end
   end
 
+  # mercury_and_tags_update
+  # allows updating a repo's tags
+  #   through put request to '/repo/:owner/:name'
+  #   params in body
+  # allows updating a repo's wiki_text
+  #   using the mercury visual editor
+  #   through put request to '/repo/:owner/:name'
+  #   params in body
   def mercury_and_tags_update
+    # owner and name in route
     repo = Repo.find_by_owner_and_name(params[:owner], params[:name])
     
+    # is it tags update or a mercury update request?
     if params["content"]
-      #mercury update
+      # it's a mercury request
       wiki_text_new = JSON.parse(params["content"])["wiki_text_edit"]["value"]
 
       puts "New wiki_text for #{repo.ident}: '#{wiki_text_new.inspect}'"
@@ -47,23 +68,30 @@ class RepoController < ApplicationController
 
       render text: "{}"
     else
-      # post request containing tags in body
+      # it's a request updating tags (most likely)
+      # each tag_context
       %w{languages frameworks solutions}.each do |context|
         t1 = params[context]
+        # split tags
         t1 = t1.split(",")
         tag_values = []
+        # each tag
         t1.each do |value|
+          # clean whitespace
           value = value.strip
+          # do here
+          # sanitize
           tag_values << value
         end
+        # update repo using update_attribute because save somehow does not cause tags to be saved
         repo.update_attribute(context, tag_values)
       end
-
+      # do: catch  errors for requests that are neither mercury nor tags
       respond_to do |format|
         format.html { redirect_to "/repo/#{repo.ident}", notice: "Tags saved." }
       end
 
-
+      # do later: user experience improvement => ajax request for updating tags
     end
   end
 
--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,6 +1,7 @@
 class Repo < ActiveRecord::Base
   attr_accessible :owner, :name, :description, :github_url, :homepage, :forks, :watchers, :last_updated
 
+  # contexts / scopes that are taggable
   attr_taggable :languages, :frameworks, :solutions
 
   validates :owner, :name, presence: true
