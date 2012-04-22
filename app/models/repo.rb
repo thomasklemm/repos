2dce7a3346d94be8d3bc9566a40852f5a579d9ea
Thomas Klemm <mail@tklemm.eu>
2012-04-22 21:11:26 +0200

    awesome documentation

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -5,17 +5,36 @@ class Repo < ActiveRecord::Base
 
   validates :owner, :name, presence: true
 
+  # repo.ident -> "thomasklemm/repos"
+  # => returns a github ident string ('not what it's officially called, is it?')
   def ident
     return "#{self.owner}/#{self.name}"
   end
 
+  # repo.update_repo
+  # => fetches current repo data from github
+  #    and updates fields that have changed
+  # 
+  # => Data: name owner description watchers forks homepage github_url last_updated
+  #
+  # used in
+  # => initialize_repo
+  # => Repo.update_repos
   def update_repo
     puts "starting to update_repo: '#{self.ident}'"
-    github = JSON.parse(Curl::Easy.perform("https://api.github.com/repos/" + self.owner + "/" + self.name).body_str)
+    github = JSON.parse(Curl::Easy.perform("https://api.github.com/repos/" + self.ident).body_str)
     
     if github["message"]
       # Something has gone wrong
+      # probably: repo does not exist
+      # => 1) incorrect information provided when adding a repo
+      # => 2) previously existing repo has had some changes (e.g. name, ownership)
       return false
+      # do: better error handling here
+      # what happens if a repo has been renamed / ownership transferred / deleted
+      # => set flag 'needs_an_eye' to true
+      # => then jump to the next repo
+      # => might be done using 'markable' gem
     else
       %w{name description watchers forks homepage}.each do |field|
         self[field] = github[field]
@@ -27,12 +46,19 @@ class Repo < ActiveRecord::Base
     end
   end
 
-
+  # repo.initialize_repo
+  # => initializes a new repo
+  # what should be given: repo.owner and repo.name
   def initialize_repo
-    puts "initialize_repo: '#{self.ident}' finished successfully."
-    self.update_repo 
+    puts "starting to initialize repo: '#{self.ident}' finished successfully."
+    self.update_repo
   end
 
+  # Repo.update_repos
+  # => rails runner "Repos.update_repos"
+  # 
+  # Cron job that will update the complete repository dataset
+  # with current github data
   def self.update_repos
   	# Find repos in batch sizes of 1000
   	Repo.find_each do |repo|
@@ -40,7 +66,6 @@ class Repo < ActiveRecord::Base
   	end
 
     puts "Job 'Repo.update_repos' finished successfully."
-
   end
 
 
