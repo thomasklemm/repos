3708827be98075f23cf7f4306df2df568e887fe9
Thomas Klemm <mail@tklemm.eu>
2012-04-25 10:37:26 +0200

    new filtrify.js

--- app/assets/javascripts/all/filtrify.js
+++ app/assets/javascripts/all/filtrify.js
@@ -1,13 +1,3 @@
-/**
- * jQuery Filtrify v0.2
- * Beautiful advanced tag filtering with HTML5 and jQuery
- * http://luis-almeida.github.com/filtrify
- *
- * Licensed under the MIT license.
- * Copyright 2012 Luís Almeida
- * https://github.com/luis-almeida
- */
-
 ;(function ( $, window, document, undefined ) {
 
 	var defaults = {
@@ -15,9 +5,9 @@
 		hide      : true,
 		block     : [],
 		close     : false,
-		query     : undefined, // { category : [tags] } }
-		callback  : undefined, // function ( query, match, mismatch ) {}
-		match     : true //show or hide no matches
+		query     : undefined, // { field1 : [tags] } }
+		callback : undefined, // function ( query, match, mismatch ) {}
+		match: false,//show or hide no matches
 	}; 
 
 	function Filtrify( containerID, placeholderID, options ) {
